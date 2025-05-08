9ed94edda13d8a5b825d1146827131338346296f
Santhosh272005 <144514823+Santhosh272005@users.noreply.github.com>
2025-05-08 10:08:29 +0530

    day6

---
+++ loop.java
@@ -0,0 +1,37 @@
+package day4;
+import java.util.Scanner;
+public class loop {
+
+	public static void main(String[] args) {
+		
+		//number count------
+		
+		/*for (int i = 1; i<=10; i++) {
+		    
+		  System.out.println(i);
+			}*/
+		
+		// name in multiple times----
+		
+		/*for (int i = 1; i<=4; i++) {
+		    
+			  System.out.println("SANTA");
+			}*/
+		
+		//tables-----
+		
+		/*Scanner get = new Scanner(System.in);
+ 		
+ 		System.out.println("Enter the table you want : ");
+ 		int value =  get.nextInt();
+ 		
+ 		for (int i = 1; i<=10; i++)
+ 		{
+ 		System.out.println(value  + "X" + i  + "=" +(value * i) );
+	}*/
+		
+		}
+
+	}
+
+

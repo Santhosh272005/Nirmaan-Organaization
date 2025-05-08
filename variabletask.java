5bffd20481301c6864a941103091b015b41e7ab5
Santhosh272005 <144514823+Santhosh272005@users.noreply.github.com>
2025-05-08 09:57:59 +0530

    day2

---
+++ variabletask.java
@@ -0,0 +1,25 @@
+package day2;
+
+public class variabletask {
+public static void main(String args[])
+{
+	int a = 3000;
+	byte b = 126;
+	short c = 12345;
+	long d = 90000000l;
+	String e = "santa";
+	float x = 12.23f;
+	double y = 12.12345;
+	char z = 'a';
+	
+	
+	System.out.println("int = " + a);
+	System.out.println("byte = " +b);
+	System.out.println("short = " +c);
+	System.out.println("long = " +d);
+	System.out.println("String = " + e);
+	System.out.println("float = " +x);
+	System.out.println("double = " +y);
+	System.out.println("char = " +z);
+}
+}
